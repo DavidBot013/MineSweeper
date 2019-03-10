465ba5addc5b6d1c608f4099951116c610bb669c
David13 <santiago.cortes0024@gmail.com>
2019-03-10 12:11:11 -0500

    Constructor y primera funcion de Control.

--- buscaMinas/Celda.java
+++ buscaMinas/Celda.java
@@ -6,20 +6,37 @@ import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+// TODO: Auto-generated Javadoc
+/**
+ * Clase modelo del juego BuscaMinas.
+ */
 public class Celda extends JButton{
-	/**
-	 * 
-	 */
+	
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
+	
+	/** The Constant SIZE. */
 	private static final int SIZE = 15;
+	
+	/** The estado. */
 	private int minasCercanas, fila, col, estado;
+	
+	/** The es mina. */
 	private boolean esMina;
+	
+	/** The color. */
 	private String color;
 	
-	public Celda(int fila, int col, boolean esMina) {
+	/**
+	 * Instancia una nueva celda.
+	 *
+	 * @param fila the fila
+	 * @param col the col
+	 * @param esMina the es mina
+	 */
+	public Celda(int fila, int col) {
 		this.fila=fila;
 		this.col=col;
-		this.esMina=esMina;
 		estado=0;
 		this.setPreferredSize(new Dimension(SIZE, SIZE));
 		this.setOpaque(true);
@@ -31,10 +48,15 @@ public class Celda extends JButton{
 	 * 0 -> Estado inicial (tapada);
 	 * 1 -> Destapada
 	 * 2 -> Marcada
-	 * @param action
+	 *
+	 * @param estado the new state
 	 */
 	public void setState(int estado) {
 		this.estado=estado;
+	}
+	
+	public void beMine() {
+		esMina=true;
 	}
 	
 	/**
@@ -46,26 +68,57 @@ public class Celda extends JButton{
 			this.setIcon(new ImageIcon("src/resources/"+minasCercanas+".png"));
 		}
 	}
+	
+	/**
+	 * Gets the minas cercanas.
+	 *
+	 * @return the minas cercanas
+	 */
 	public int getMinasCercanas() {
 		return minasCercanas;
 	}
 
+	/**
+	 * Gets the fila.
+	 *
+	 * @return the fila
+	 */
 	public int getFila() {
 		return fila;
 	}
 
+	/**
+	 * Gets the col.
+	 *
+	 * @return the col
+	 */
 	public int getCol() {
 		return col;
 	}
 
+	/**
+	 * Gets the estado.
+	 *
+	 * @return the estado
+	 */
 	public int getEstado() {
 		return estado;
 	}
 
+	/**
+	 * Checks if is mina.
+	 *
+	 * @return true, if is mina
+	 */
 	public boolean isMina() {
 		return esMina;
 	}
 
+	/**
+	 * Gets the color.
+	 *
+	 * @return the color
+	 */
 	public String getColor() {
 		return color;
 	}	
--- buscaMinas/Control.java
+++ buscaMinas/Control.java
@@ -1,5 +1,22 @@
 package buscaMinas;
 
 public class Control {
-
+	private Celda[][] celdas;
+	private boolean ganador;
+	private static final int MINAS = 40;
+	private static final int BOARD = 16;
+	
+	public Control() {
+		celdas = new Celda[BOARD][BOARD];
+		ganador=false;
+		instanciarCeldas();
+	}
+	
+	private void instanciarCeldas() {
+		for(int fila=0; fila<BOARD; fila++) {
+			for(int col=0; col<BOARD; col++) {
+				celdas[fila][col] = new Celda(fila, col);
+			}
+		}
+	}
 }
